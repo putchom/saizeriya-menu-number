7865eb0061584985770fc5e3015050f6b6fda8a7
putchom <putchom8@gmail.com>
2023-12-08 10:20:56 +0900

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,49 @@ import { useState } from "react";
 import "./App.css";
 
 interface Image {
-  id: number;
+  id: string;
   path: string;
+}
+
+enum GameStatus {
+  START,
+  PLAYING,
+  END,
 }
 
 const App = () => {
   const [images] = useState<Image[]>([
-    { id: 101, path: "image1.jpg" },
-    { id: 102, path: "image2.jpg" },
-    { id: 103, path: "image3.jpg" },
-    { id: 104, path: "image4.jpg" },
-    { id: 105, path: "image5.jpg" },
-    { id: 106, path: "image6.jpg" },
-    { id: 107, path: "image7.jpg" },
-    { id: 108, path: "image8.jpg" },
-    { id: 109, path: "image9.jpg" },
-    { id: 110, path: "image10.jpg" },
-    { id: 111, path: "image11.jpg" },
-    { id: 112, path: "image12.jpg" },
-    { id: 113, path: "image13.jpg" },
-    { id: 114, path: "image14.jpg" },
-    { id: 115, path: "image15.jpg" },
-    { id: 116, path: "image16.jpg" },
-    { id: 117, path: "image17.jpg" },
-    { id: 118, path: "image18.jpg" },
-    { id: 119, path: "image19.jpg" },
-    { id: 120, path: "image20.jpg" },
+    { id: "101", path: "image1.jpg" },
+    { id: "102", path: "image2.jpg" },
+    { id: "103", path: "image3.jpg" },
+    { id: "104", path: "image4.jpg" },
+    { id: "105", path: "image5.jpg" },
+    { id: "106", path: "image6.jpg" },
+    { id: "107", path: "image7.jpg" },
+    { id: "108", path: "image8.jpg" },
+    { id: "109", path: "image9.jpg" },
+    { id: "110", path: "image10.jpg" },
+    { id: "111", path: "image11.jpg" },
+    { id: "112", path: "image12.jpg" },
+    { id: "113", path: "image13.jpg" },
+    { id: "114", path: "image14.jpg" },
+    { id: "115", path: "image15.jpg" },
+    { id: "116", path: "image16.jpg" },
+    { id: "117", path: "image17.jpg" },
+    { id: "118", path: "image18.jpg" },
+    { id: "119", path: "image19.jpg" },
+    { id: "120", path: "image20.jpg" },
   ]);
   const [selectedImages, setSelectedImages] = useState<Image[]>([]);
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
-  const [gameStarted, setGameStarted] = useState<boolean>(false);
+  const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.START);
   const [userInput, setUserInput] = useState<string>("");
   const [tweetText, setTweetText] = useState<string>("");
-  const [currentInputId, setCurrentInputId] = useState<number | null>(null);
+  const [currentInputId, setCurrentInputId] = useState<string | null>(null); // 文字列型に変更
   const [answerFeedback, setAnswerFeedback] = useState<string | null>(null);
+  const [correctAnswers, setCorrectAnswers] = useState<Image[]>([]); // 正解した項目を格納する配列
+  const [incorrectAnswers, setIncorrectAnswers] = useState<Image[]>([]); // 不正解だった項目を格納する配列
 
   const selectRandomImages = (): Image[] => {
     const selected: Image[] = [];
@@ -53,36 +61,37 @@ const App = () => {
   const startGame = (): void => {
     const newSelectedImages = selectRandomImages();
     setSelectedImages(newSelectedImages);
-    setGameStarted(true);
+    setGameStatus(GameStatus.PLAYING);
     setCurrentImageIndex(0);
     setScore(0);
     setTweetText("");
     setCurrentInputId(newSelectedImages[0].id);
     setAnswerFeedback(null);
-    // デバッグ用に現在のIDを出力
+
     console.log("現在のID: ", newSelectedImages[0].id);
   };
 
   const checkAnswer = (): void => {
     const currentImage = selectedImages[currentImageIndex];
-    if (currentImage && parseInt(userInput) === currentImage.id) {
+    if (currentImage && userInput === currentImage.id) {
       setScore(score + 1);
-      setAnswerFeedback("正解！");
+      setCorrectAnswers([...correctAnswers, currentImage]);
     } else {
-      setAnswerFeedback("不正解");
+      setIncorrectAnswers([...incorrectAnswers, currentImage]);
     }
     setCurrentImageIndex(currentImageIndex + 1);
 
     if (currentImageIndex < selectedImages.length - 1) {
       setCurrentInputId(selectedImages[currentImageIndex + 1].id);
-      // デバッグ用に現在のIDを出力
       console.log("現在のID: ", selectedImages[currentImageIndex + 1]?.id);
     }
 
     if (currentImageIndex === selectedImages.length - 1) {
-      setGameStarted(false);
+      setGameStatus(GameStatus.END);
       setTweetText(`私の得点は ${score} 点でした！ #ImageGame`);
     }
+
+    setUserInput("");
   };
 
   const tweetScore = (): void => {
@@ -92,15 +101,24 @@ const App = () => {
     window.open(tweetUrl, "_blank");
   };
 
+  const restartGame = (): void => {
+    startGame();
+    setCorrectAnswers([]);
+    setIncorrectAnswers([]);
+  };
+
   return (
     <div>
-      <h1>画像を見て正しいIDを当てよう！</h1>
-      {!gameStarted && (
+      <div>
+        <h1>フラッシュサイゼリヤ</h1>
+      </div>
+      {gameStatus === GameStatus.START && (
         <div>
-          <button onClick={startGame}>ゲーム開始</button>
+          <h2>サイゼリヤのメニューの写真を見て注文IDを当てよう</h2>
+          <button onClick={startGame}>プレイ</button>
         </div>
       )}
-      {gameStarted && (
+      {gameStatus === GameStatus.PLAYING && (
         <div>
           <p>現在の問題: {currentImageIndex + 1} / 5</p>
           <img
@@ -115,15 +133,46 @@ const App = () => {
               value={userInput}
               onChange={(e) => setUserInput(e.target.value)}
             />
-            <button onClick={checkAnswer}>確認</button>
+            <button onClick={checkAnswer}>次へ</button>{" "}
           </p>
           {answerFeedback && <p>{answerFeedback}</p>}
         </div>
       )}
-      {currentImageIndex === selectedImages.length && (
+      {gameStatus === GameStatus.END && (
         <div>
           <p>得点: {score}</p>
           <button onClick={tweetScore}>得点をツイート</button>
+          <button onClick={restartGame}>もう一度プレイ</button>
+          <div>
+            <h2>正解</h2>
+            <ul>
+              {correctAnswers.map((item) => (
+                <li key={item.id}>
+                  <img
+                    src={item.path}
+                    alt={`ID: ${item.id}`}
+                    width="100"
+                    height="75"
+                  />
+                  <span>ID: {item.id}</span>
+                </li>
+              ))}
+            </ul>
+            <h2>不正解</h2>
+            <ul>
+              {incorrectAnswers.map((item) => (
+                <li key={item.id}>
+                  <img
+                    src={item.path}
+                    alt={`ID: ${item.id}`}
+                    width="100"
+                    height="75"
+                  />
+                  <span>ID: {item.id}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       )}
     </div>
