f3cfb535d0c356d90f803324fc99d9cdc36fd09f
putchom <putchom8@gmail.com>
2023-12-10 13:22:18 +0900

    test App

--- src/App.tsx
+++ src/App.tsx
@@ -20,3 +20,54 @@ const App = () => {
 };
 
 export default App;
+
+if (import.meta.vitest) {
+  const { describe, test, expect } = import.meta.vitest;
+  const { RecoilRoot } = await import("recoil");
+  const { render } = await import("@testing-library/react");
+  const { gameStatusState } = await import("./states");
+
+  describe("App", () => {
+    test("gameStatusがSTARTのときStartPageが描画される", async () => {
+      const mockedState = GameStatus.START;
+      const { findByText } = render(
+        <RecoilRoot
+          initializeState={(snapshot) =>
+            snapshot.set(gameStatusState, mockedState)
+          }
+        >
+          <App />
+        </RecoilRoot>,
+      );
+      expect(await findByText("プレイ")).not.toBeNull();
+    });
+
+    test("gameStatusがPLAYINGのときPlayingPageが描画される", async () => {
+      const mockedState = GameStatus.PLAYING;
+      const { findByText } = render(
+        <RecoilRoot
+          initializeState={(snapshot) =>
+            snapshot.set(gameStatusState, mockedState)
+          }
+        >
+          <App />
+        </RecoilRoot>,
+      );
+      expect(await findByText("回答")).not.toBeNull();
+    });
+
+    test("gameStatusがENDのときEndPageが描画される", async () => {
+      const mockedState = GameStatus.END;
+      const { findByText } = render(
+        <RecoilRoot
+          initializeState={(snapshot) =>
+            snapshot.set(gameStatusState, mockedState)
+          }
+        >
+          <App />
+        </RecoilRoot>,
+      );
+      expect(await findByText("トップに戻る")).not.toBeNull();
+    });
+  });
+}
