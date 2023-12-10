8d6763a0c67298c7163aea0261bbc164175d6ac1
putchom <putchom8@gmail.com>
2023-12-10 12:28:27 +0900

    vitest

--- src/utils/selectRandomMeals.ts
+++ src/utils/selectRandomMeals.ts
@@ -14,3 +14,29 @@ export const selectRandomMeals = (args: {
   }
   return selected;
 };
+
+if (import.meta.vitest) {
+  const { describe, test, expect } = import.meta.vitest;
+
+  describe("selectRandomMeals", () => {
+    const meals = [
+      { id: "1", name: "Meal 1", imagePath: "/" },
+      { id: "2", name: "Meal 2", imagePath: "/" },
+      { id: "3", name: "Meal 3", imagePath: "/" },
+      { id: "4", name: "Meal 4", imagePath: "/" },
+      { id: "5", name: "Meal 5", imagePath: "/" },
+    ];
+
+    test("指定した個数のMealsが返る", () => {
+      const selected = selectRandomMeals({ meals, quantity: 3 });
+      expect(selected.length).toBe(3);
+    });
+
+    test("Mealsの中に同じMealが含まれない", () => {
+      const selected = selectRandomMeals({ meals, quantity: 3 });
+      expect(selected[0]).not.toBe(selected[1]);
+      expect(selected[0]).not.toBe(selected[2]);
+      expect(selected[1]).not.toBe(selected[2]);
+    });
+  });
+}
